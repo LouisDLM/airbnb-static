c7deb665c95d8b695a6912a616135b8fb07f4d1d
Louis de La Marnierre <empsalle@gmail.com>
2017-06-26 16:29:04 -0400

    Done bruh

--- config.rb
+++ config.rb
@@ -83,9 +83,17 @@ activate :deploy do |deploy|
   deploy.method = :git
   deploy.build_before = true
 
+
   # Optional Settings
   # deploy.remote = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
+
+
+
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
 end
